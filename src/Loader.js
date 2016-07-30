7e50187fac702c2ce3d531723726e8865e621380
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2016-07-30 12:51:48 +0430

    messaging is gone

--- src/Loader.js
+++ src/Loader.js
@@ -5,14 +5,6 @@ Loader.directive("loader" , function () {
         replace : true ,
         template : "<div " +
         "ng-class='{loaderDisplay : loaderDisplay}' class='loaderWrapper'>" +
-        "<div " +
-
-        "ng-class='{" +
-        "loaderMessageDisplay : loaderMessageDisplay == true , " +
-        "slide : loaderMessageDisplay == -1}' class='loaderMessage'>" +
-        " {{ loaderMessage }}" +
-        " </div>" +
-
         "<div class='dot dot1'></div> " +
         "<div class='dot dot2'></div>" +
         "<div class='dot dot3'></div>" +
@@ -38,7 +30,6 @@ Loader.directive("loader" , function () {
 
 Loader.factory("Loader" , function ( $rootScope , $timeout ) {
     var _timeOut  ;
-    $rootScope.loaderMessage = null ;
     $rootScope.loaderDisplay = false ;
     $rootScope.loaderStyle = {
         bg : null ,
@@ -46,51 +37,21 @@ Loader.factory("Loader" , function ( $rootScope , $timeout ) {
         _class : null
     } ;
 
-    var start = function (timeout , message ) {
+    var start = function (timeout) {
 
         $timeout.cancel(_timeOut) ;
 
-        $rootScope.loaderMessage = message ;
         $rootScope.loaderDisplay = true ;
-        if ( message ) $rootScope.loaderMessageDisplay = true ;
 
         if ( timeout ) {
             _timeOut = $timeout(function () {
                 $rootScope.loaderDisplay = false ;
-                $rootScope.loaderMessageDisplay = false ;
             } ,  timeout ) ;
         }
     } ;
 
     var dismiss = function () {
         $rootScope.loaderDisplay = false ;
-        $rootScope.loaderMessageDisplay = false ;
-    } ;
-
-    var message = function (message) {
-        // if any Loader is currently Displayed
-        if ( $rootScope.loaderDisplay ) {
-
-            // if a message already exists on screen
-            if ( $rootScope.loaderMessageDisplay ) {
-
-                // hide that message and show the new one
-                $rootScope.loaderMessageDisplay = false ;
-                $timeout(function () {
-                    $rootScope.loaderMessage = message  ;
-                    $rootScope.loaderMessageDisplay = true ;
-                } , 500 )
-
-            }
-            else {
-                // just show the main thing
-                $rootScope.loaderMessage = message ;
-                $rootScope.loaderMessageDisplay = true ;
-            }
-        }
-        else {
-            console.warn("Loader Module : Loader Not Displayed . No Message Will Be Displayed")
-        }
     } ;
 
     return {
--- test/app.js
+++ test/app.js
@@ -3,14 +3,9 @@ var app = angular.module("app" , ["Loader"]) ;
 app.controller("Ctrl" , function ($scope , Loader , $timeout ) {
     $timeout(function () {
         Loader.start() ;
-
-        $timeout(function () {
-            Loader.message("Some Message") ;
-        } , 2000 ) ;
-
-        $timeout(function () {
-            Loader.message("Some Other Text" )
-        } , 3000 ) ;
-
     } , 1000 ) ;
+
+    $timeout(function() {
+      Loader.dismiss()
+    }, 10000)
 }) ;
